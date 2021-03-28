27d3edb5e69bc56c943c0fa2fffad4483fdb6c0b
MATSUBARA Nobutada <nobutada0507@gmail.com>
2021-03-29 03:43:04 +0900

    Fix: add exposing (Set) to defElmImports

--- app/generate/Main.hs
+++ app/generate/Main.hs
@@ -3,12 +3,13 @@
 module Main where
 
 import           RIO
+import qualified RIO.Text            as Text
 
 import           Elm.Mapping
 import           Homely.API          (CRUD)
 import           Homely.Data.Expense (Expense, Label)
 import           Servant             ((:>))
-import           Servant.Elm.Mapping
+import           Servant.Elm.Mapping hiding (defElmImports)
 import           System.Environment  (getArgs)
 
 main :: IO ()
@@ -23,3 +24,17 @@ main = do
     , DefineElm (Proxy @ Label)
     ]
     (Proxy @ ("api" :> CRUD))
+
+defElmImports :: Text
+defElmImports =
+  Text.unlines
+    [ "import Json.Decode"
+    , "import Json.Encode exposing (Value)"
+    , "-- The following module comes from bartavelle/json-helpers"
+    , "import Json.Helpers exposing (..)"
+    , "import Dict exposing (Dict)"
+    , "import Set exposing (Set)"
+    , "import Http"
+    , "import String"
+    , "import Url.Builder"
+    ]
